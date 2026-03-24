516931f134a5f2d0f961467850abae972d1a28ba
Anand Shukla <anands2807@gmail.com>
2026-03-24 13:37:15 +0530

    credential true

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,8 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use(cors({
-  origin: process.env.CLIENT_URL
+  origin: process.env.CLIENT_URL,
+  credentials: true,
 }));
 
 app.get('/', (req, res)=>{
