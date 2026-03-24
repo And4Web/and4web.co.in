180efa40ad128bcbd866f2e1e3ae28faa2f14b92
Anand Shukla <anands2807@gmail.com>
2026-03-24 13:23:09 +0530

    test route

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+
+app.use(cors({
+  origin: process.env.CLIENT_URL
+}));
+
 app.get('/', (req, res)=>{
   res.send("hello world!")
+})
+
+app.get('/test', (req, res)=>{
+  res.status(200).json({"message": "test successful"})
 })
 
 app.listen(PORT, ()=>{
