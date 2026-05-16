fde357908647c760dd33ca1b5f6c5f7790fe8f83
Anand Shukla <anands2807@gmail.com>
2026-05-16 12:17:51 +0530

    Dropdown added with some minor issues check it

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ function privateMessage(){
 
   fetch(`${api_base_url}/test`).then(response=>response.json()).then(data=>console.log("data from server >>> ", data)).catch(e=>console.log("Error connecting to the server >>> ", e));
 }
+
+
+// Dropdown function
+const dropdownBtn = document.getElementById("dropdown-btn");
+const dropdownContent = document.getElementById("dropdown-content");
+const navbar = document.getElementById("navbar");
+function dropdown(){
+  console.log("dropdown...")
+  if(dropdownContent.classList.contains("dropdown-active")){
+    dropdownContent.classList.remove("dropdown-active")
+  }else{
+    dropdownContent.classList.add('dropdown-active')
+  }
+  // if(navbar.style.display = "block"){
+  //   dropdownContent.classList.remove('dropdown-active');
+  // }
+}
